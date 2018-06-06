7ab2ef3d63cb6e8f2608041225b660ae5069338c
HernanRossi <hernan_rossi@msn.com>
2018-06-06 00:24:13 -0700

    updated naive bayes

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,6 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import re
+import math
 class NaiveBayes:
 
     def __init__(self):
@@ -20,23 +21,30 @@ class NaiveBayes:
         negativeFiles = [f for f in listdir('C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\negative')
                      if isfile(join('C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\negative', f))]
         for file in negativeFiles:
+            dictionaryWordCurrent = ['awful', 'bad', 'boring', 'dull',
+                               'effective', 'enjoyable', 'great', 'hilarious']
             location = 'C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\negative\\' + file
             readFile = open(location, 'r')
             fileWords = re.findall(r"[\w]+", readFile.read())
-
             for i in range(0, len(fileWords)):
-                self.wordCount += 1
-                if fileWords[i].lower() in self.dicWordCounts:
-                    # print('Word found ', fileWords[i].lower())
-                    self.dicWordCounts[fileWords[i].lower()] += 1
+                # self.wordCount += 1
+                # if fileWords[i].lower() in self.dicWordCounts:
+                #     # print('Word found ', fileWords[i].lower())
+                #     self.dicWordCounts[fileWords[i].lower()] += 1
+                for word in dictionaryWordCurrent:
+                    if fileWords[i].lower() == word:
+                        self.dicWordCounts[word] += 1
+                        dictionaryWordCurrent.remove(word)
         print('Negative Reviews')
-        print('Word count: ', self.wordCount)
+        # print('Docs with terms: ', self.wordCount)
         print(self.dicWordCounts)
         for word in self.dicWordCounts:
-            probability = self.dicWordCounts[word]/self.wordCount
-            self.conditionalProbabiltiesNegative[word] = probability/0.5
+            probability = (self.dicWordCounts[word] + 1)/1002
+            # probability = self.dicWordCounts[word]/self.wordCount
+            self.conditionalProbabiltiesNegative[word] = probability
+            # self.conditionalProbabiltiesNegative[word] = probability/0.5
             # print(word, ' appeared ', self.dicWordCounts[word], ' P( word And class)= ', probability , ' P(word | class) = ', probability/ 0.5)
-        # print(self.conditionalProbabiltiesNegative)
+        print(self.conditionalProbabiltiesNegative)
 
 
         self.wordCount = 0
@@ -46,20 +54,28 @@ class NaiveBayes:
             location = 'C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\positive\\' + file
             readFile = open(location, 'r')
             fileWords = re.findall(r"[\w]+", readFile.read())
+            dictionaryWordCurrent = ['awful', 'bad', 'boring', 'dull',
+                                     'effective', 'enjoyable', 'great', 'hilarious']
             for i in range(0, len(fileWords)):
-                self.wordCount += 1
-                for dicWord in self.dictionaryWords:
-                    if fileWords[i].lower() == dicWord:
-                        # print('Word found ', fileWords[i].lower())
-                        self.dicWordCounts[fileWords[i].lower()] += 1
+                # self.wordCount += 1
+                # for dicWord in self.dictionaryWords:
+                #     if fileWords[i].lower() == dicWord:
+                #         # print('Word found ', fileWords[i].lower())
+                #         self.dicWordCounts[fileWords[i].lower()] += 1
+                for word in dictionaryWordCurrent:
+                    if fileWords[i].lower() == word:
+                        self.dicWordCounts[word] += 1
+                        dictionaryWordCurrent.remove(word)
         print('Positive Words')
-        print('Word count: ', self.wordCount)
+        # print('Word count: ', self.wordCount)
         print(self.dicWordCounts)
         for word in self.dicWordCounts:
-            probability = self.dicWordCounts[word] / self.wordCount
-            self.conditionalProbabiltiesPostive[word] = probability/0.5
+            probability = (self.dicWordCounts[word] +1) / 1002
+            # probability = self.dicWordCounts[word]/self.wordCount
+            self.conditionalProbabiltiesPostive[word] = probability
+            # self.conditionalProbabiltiesPostive[word] = probability/0.5
             # print(word, ' appeared ', self.dicWordCounts[word], ' P( word And class)= ', probability , ' P(word | class) = ', probability/ 0.5)
-        # print(self.conditionalProbabiltiesPostive)
+        print(self.conditionalProbabiltiesPostive)
 
 
     def startNaiveBayes(self):
@@ -71,8 +87,8 @@ class NaiveBayes:
                          if isfile(join('C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\positive', f))]
         negativeFiles = [f for f in listdir('C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\negative')
                          if isfile(join('C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\negative', f))]
-        probNegative = 0
-        probPositive = 0
+        probNegative = math.log(0.5)
+        probPositive = math.log(0.5)
         predictedNegativeCount = 0
         predictedPositiveeCount = 0
         for file in negativeFiles:
@@ -86,27 +102,34 @@ class NaiveBayes:
                             featureVector[j] = 1
                 for (n,m) in enumerate(self.conditionalProbabiltiesNegative):
                     # print(n,m)
-                    if probNegative == 0:
-                        probNegative = (self.conditionalProbabiltiesNegative[m]**featureVector[n] ) * (
-                                (1 -self.conditionalProbabiltiesNegative[m])**(1-featureVector[n]) )
+                    # if probNegative == 0:
+                    #     probNegative = (self.conditionalProbabiltiesNegative[m]**featureVector[n] ) * (
+                    #             (1 -self.conditionalProbabiltiesNegative[m])**(1-featureVector[n]) )
+                    # else:
+                    #     probNegative *= (self.conditionalProbabiltiesNegative[m]**featureVector[n] ) * (
+                    #             (1 -self.conditionalProbabiltiesNegative[m])**(1-featureVector[n]) )
+                    if featureVector[n] == 1:
+                        probNegative += math.log(self.conditionalProbabiltiesNegative[m])
                     else:
-                        probNegative *= (self.conditionalProbabiltiesNegative[m]**featureVector[n] ) * (
-                                (1 -self.conditionalProbabiltiesNegative[m])**(1-featureVector[n]) )
-
+                        probNegative += math.log(1-self.conditionalProbabiltiesNegative[m])
                 for (n,m) in enumerate(self.conditionalProbabiltiesPostive):
                     # print(n,m)
-                    if probPositive == 0:
-                        probPositive = (self.conditionalProbabiltiesPostive[m]**featureVector[n] ) * (
-                                (1 -self.conditionalProbabiltiesPostive[m])**(1-featureVector[n]) )
+                    # if probPositive == 0:
+                    #     probPositive = (self.conditionalProbabiltiesPostive[m]**featureVector[n] ) * (
+                    #             (1 -self.conditionalProbabiltiesPostive[m])**(1-featureVector[n]) )
+                    # else:
+                    #     probPositive *= (self.conditionalProbabiltiesPostive[m]**featureVector[n] ) * (
+                    #             (1 -self.conditionalProbabiltiesPostive[m])**(1-featureVector[n]) )
+                    if featureVector[n] == 1:
+                        probPositive += math.log(self.conditionalProbabiltiesPostive[m])
                     else:
-                        probPositive *= (self.conditionalProbabiltiesPostive[m]**featureVector[n] ) * (
-                                (1 -self.conditionalProbabiltiesPostive[m])**(1-featureVector[n]) )
+                        probPositive += math.log(1 - self.conditionalProbabiltiesPostive[m])
 
                 if probNegative > probPositive:
                     predictedNegativeCount +=1
         print('Predicted ', predictedNegativeCount, ' files were negative when 1000 were actually negative')
-        probNegative = 0
-        probPositive = 0
+        probNegative = math.log(0.5)
+        probPositive = math.log(0.5)
         for file in positiveFiles:
             location = 'C:\\Users\\herna\\PycharmProjects\\A2\\reviewFiles\\positive\\' + file
             readFile = open(location, 'r')
@@ -118,24 +141,28 @@ class NaiveBayes:
                         featureVector[j] = 1
             for (n, m) in enumerate(self.conditionalProbabiltiesNegative):
                 # print(n,m)
-                if probNegative == 0:
-                    probNegative = (self.conditionalProbabiltiesNegative[m] ** featureVector[n]) * (
-                                (1 - self.conditionalProbabiltiesNegative[m]) ** (1 - featureVector[n]))
+                # if probNegative == 0:
+                #     probNegative = (self.conditionalProbabiltiesNegative[m]**featureVector[n] ) * (
+                #             (1 -self.conditionalProbabiltiesNegative[m])**(1-featureVector[n]) )
+                # else:
+                #     probNegative *= (self.conditionalProbabiltiesNegative[m]**featureVector[n] ) * (
+                #             (1 -self.conditionalProbabiltiesNegative[m])**(1-featureVector[n]) )
+                if featureVector[n] == 1:
+                    probNegative += math.log(self.conditionalProbabiltiesNegative[m])
                 else:
-                    probNegative *= (self.conditionalProbabiltiesNegative[m] ** featureVector[n]) * (
-                                (1 - self.conditionalProbabiltiesNegative[m]) ** (1 - featureVector[n]))
-
+                    probNegative += math.log(1 - self.conditionalProbabiltiesNegative[m])
             for (n, m) in enumerate(self.conditionalProbabiltiesPostive):
                 # print(n,m)
-                if probPositive == 0:
-                    probPositive = (self.conditionalProbabiltiesPostive[m] ** featureVector[n]) * (
-                                (1 - self.conditionalProbabiltiesPostive[m]) ** (1 - featureVector[n]))
+                # if probPositive == 0:
+                #     probPositive = (self.conditionalProbabiltiesPostive[m]**featureVector[n] ) * (
+                #             (1 -self.conditionalProbabiltiesPostive[m])**(1-featureVector[n]) )
+                # else:
+                #     probPositive *= (self.conditionalProbabiltiesPostive[m]**featureVector[n] ) * (
+                #             (1 -self.conditionalProbabiltiesPostive[m])**(1-featureVector[n]) )
+                if featureVector[n] == 1:
+                    probPositive += math.log(self.conditionalProbabiltiesPostive[m])
                 else:
-                    probPositive *= (self.conditionalProbabiltiesPostive[m] ** featureVector[n]) * (
-                                (1 - self.conditionalProbabiltiesPostive[m]) ** (1 - featureVector[n]))
-            #
-            # print('Prob file is negative ', probNegative)
-            # print('Porb file is positive ',probPositive)
+                    probPositive += math.log(1 - self.conditionalProbabiltiesPostive[m])
             if probPositive > probNegative:
                 predictedPositiveeCount += 1
         print('Predicted ', predictedPositiveeCount, ' files were positive when 1000 were actually positive')
